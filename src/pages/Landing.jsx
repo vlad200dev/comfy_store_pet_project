e9feaa3cea91f00eefc8b93eee0dc61a3574ee6a
kvlad <kutvladislaw@gmail.com>
2023-11-19 21:30:13 +0600

    landing basics page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Hero} from "../components/index.js";
 
 const Landing = () => {
     return (
-        <h1 className={'text-4xl'}>
-            Landing
-        </h1>
+        <>
+            <Hero/>
+        </>
     );
 };
 
