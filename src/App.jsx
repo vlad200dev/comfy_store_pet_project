f16247ef3aaa3e321432e281dc9b7e1feff608a4
kvlad <kutvladislaw@gmail.com>
2023-11-21 21:12:47 +0600

    checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,13 @@ import {ErrorElement} from "./components/index.js";
 import {loader as landingLoader} from './pages/Landing';
 import {loader as singleProductLoader} from './pages/SingleProduct';
 import {loader as productsLoader} from './pages/Products';
+import { loader as checkoutLoader } from './pages/Checkout';
 
 //actions
 import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
 import { store } from './store';
+import { action as checkoutAction } from './components/CheckoutForm';
 
 
 const router = createBrowserRouter([
@@ -59,7 +61,9 @@ const router = createBrowserRouter([
             },
             {
                 path: 'checkout',
-                element: <Checkout/>
+                element: <Checkout/>,
+                loader: checkoutLoader(store),
+                action: checkoutAction(store),
             },
             {
                 path: 'orders',
--- src/components/index.js
+++ src/components/index.js
@@ -18,3 +18,4 @@ export {default as Loading} from './Loading';
 export {default as CartTotals} from './CartTotals';
 export {default as CartItemsList} from './CartItemsList';
 export {default as CartItem} from './CartItem';
+export {default as CheckoutForm} from './CheckoutForm';
